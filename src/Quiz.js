15415032639b9b63b67f8fcc1476a7042224eb7f
Cristina Tinetti <cristinatinetti@hotmail.com>
2018-09-07 14:36:24 -0600

    07 sep (2)
    quiz questions and handling events

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,16 +5,24 @@ import QuizEnd from './QuizEnd.js'
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = { quiz_position: 1 }
+    }
+    showNextQuestion() {
+        this.setState((state => {
+            return { quiz_position: state.quiz_position + 1 }
+        }))
     }
     render() {
         const isQuizEnd = ((this.state.quiz_position -1 )===quizData.quiz_questions.length)
         return (
             <div>
                 { isQuizEnd ? <QuizEnd /> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+                <QuizQuestion 
+                    quiz_question = { quizData.quiz_questions[this.state.quiz_position - 1] } 
+                    showNextQuestionHandler = { this.showNextQuestion.bind(this) }
+                />
                 }
             </div>
         )
